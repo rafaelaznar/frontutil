5979e56bab89abced60851ba1ae321758d2f81e9
Ariel Sempertegui <arielsempertegu11@gmail.com>
2025-11-25 23:26:34 +0100

    Second commit

---
+++ src/app/model/sempertegui.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ServiceNameService {
+    constructor(private httpClient: HttpClient) { }
+    
+    getPeliculas(){
+        return this.httpClient.get('url');
+    }
+}
