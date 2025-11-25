4e528d333f7aa2b02698b07e5d356ab3ef61a6df
NoSoyVerde <hectorkotor@gmail.com>
2025-11-25 10:31:28 +0100

    Editado home.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,13 @@ import { RoutedUserView } from './component/blog/routed-user-view/routed-user-vi
 import { RoutedAdminEdit } from './component/blog/routed-admin-edit/routed-admin-edit';
 import { RoutedAdminNew } from './component/blog/routed-admin-new/routed-admin-new';
 import { RoutedAdminRemove } from './component/blog/routed-admin-remove/routed-admin-remove';
-import { fernandezIdeaRoutes } from './fernandez-idea.routes';
+import { FernandezRoutedAdminPlist } from './component/fernandez-idea/routed-admin-plist/routed-admin-plist';
+import { FernandezRoutedAdminView } from './component/fernandez-idea/routed-admin-view/routed-admin-view';
+import { FernandezRoutedAdminEdit } from './component/fernandez-idea/routed-admin-edit/routed-admin-edit';
+import { FernandezRoutedAdminNew } from './component/fernandez-idea/routed-admin-new/routed-admin-new';
+import { FernandezRoutedAdminRemove } from './component/fernandez-idea/routed-admin-remove/routed-admin-remove';
+import { FernandezRoutedUserPlist } from './component/fernandez-idea/routed-user-plist/routed-user-plist';
+import { FernandezRoutedUserView } from './component/fernandez-idea/routed-user-view/routed-user-view';
 
 
 export const routes: Routes = [
@@ -19,5 +25,14 @@ export const routes: Routes = [
   { path: 'blog/new', component: RoutedAdminNew },
   { path: 'blog/edit/:id', component: RoutedAdminEdit },
   { path: 'blog/remove/:id', component: RoutedAdminRemove },
-  ...fernandezIdeaRoutes,
+  // Rutas de Fernandez Ideas - Administración
+  { path: 'fernandez-idea/admin/plist', component: FernandezRoutedAdminPlist },
+  { path: 'fernandez-idea/admin/view/:id', component: FernandezRoutedAdminView },
+  { path: 'fernandez-idea/admin/new', component: FernandezRoutedAdminNew },
+  { path: 'fernandez-idea/admin/edit/:id', component: FernandezRoutedAdminEdit },
+  { path: 'fernandez-idea/admin/remove/:id', component: FernandezRoutedAdminRemove },
+  // Rutas de Fernandez Ideas - Usuario
+  { path: 'fernandez-idea/user/plist', component: FernandezRoutedUserPlist },
+  { path: 'fernandez-idea/user/view/:id', component: FernandezRoutedUserView },
+  { path: 'fernandez-idea', redirectTo: 'fernandez-idea/user/plist', pathMatch: 'full' },
 ];
