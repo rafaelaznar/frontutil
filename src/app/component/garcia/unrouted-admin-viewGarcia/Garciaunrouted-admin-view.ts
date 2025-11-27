ac81c40cbf266e088f1d8b18920b9b3e93cfc028
Olga Garcia <olgarciaq11@gmail.com>
2025-11-27 08:51:07 +0100

    todo puesto con lo mio

--- src/app/component/garcia/unrouted-admin-viewGarcia/Garciaunrouted-admin-view.ts
+++ src/app/component/garcia/unrouted-admin-viewGarcia/Garciaunrouted-admin-view.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IGarcia } from '../../../model/garcia/garcia';
 
 @Component({
   selector: 'app-Garciaunrouted-admin-view',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './Garciaunrouted-admin-view.css',
 })
 export class UnroutedAdminViewGarcia {
-
+  @Input() oGarcia: IGarcia | null = null;
 }
