e2ea2e717225b0cbd5a04d43fb15827aa39dee89
Olga <olgarciaq11@gmail.com>
2025-11-27 20:39:34 +0100

    otro mas

--- src/app/component/garcia/unrouted-admin-viewGarcia/Garciaunrouted-admin-view.ts
+++ src/app/component/garcia/unrouted-admin-viewGarcia/Garciaunrouted-admin-view.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { IGarcia } from '../../../model/garcia/garcia';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-Garciaunrouted-admin-view',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './Garciaunrouted-admin-view.html',
   styleUrl: './Garciaunrouted-admin-view.css',
 })
