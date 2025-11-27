dd6a352c20cbb8afe59a52f3db6e4e87e4fb22c6
Olga <olgarciaq11@gmail.com>
2025-11-27 20:26:26 +0100

    PORFIN EL BOTON DEL POST ID

--- src/app/component/garcia/unrouted-user-viewGarcia/Garciaunrouted-user-view.ts
+++ src/app/component/garcia/unrouted-user-viewGarcia/Garciaunrouted-user-view.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { IGarcia } from '../../../model/garcia/garcia';
 import { DatetimePipe } from "../../../pipe/datetime-pipe";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-Garciaunrouted-user-view',
-  imports: [DatetimePipe],
+  imports: [DatetimePipe, RouterLink],
   templateUrl: './Garciaunrouted-user-view.html',
   styleUrl: './Garciaunrouted-user-view.css',
 })
